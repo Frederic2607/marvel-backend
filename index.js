c1962c1e77402009240d8912a963d5069dba4ffd
Frédéric <desseaux_frederic@yahoo.fr>
2021-08-17 16:00:55 +0200

    mongoose commented

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 app.use(formidable());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
 
 const comicsRoutes = require("./routes/comics");
 const charactersRoutes = require("./routes/characters");
